c03bd439b27e98e55de1d8af51065cab3e805e64
wnqlsdl63 <wnqlsdl63@naver.com>
2021-09-13 15:03:42 +0900

    add blockhound(blocking code detect) dependency, devtools dependency and add log code

--- src/main/java/com/jubin/springboot/reactive/JubinSpringBootApplication.java
+++ src/main/java/com/jubin/springboot/reactive/JubinSpringBootApplication.java
@@ -2,11 +2,22 @@ package com.jubin.springboot.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class JubinSpringBootApplication {
 
 	public static void main(String[] args) {
+		// blocking code detect
+		// BlockHound.install();
+
+		// blocking code detect, allow Thymeleaf TemplateEngine.process method
+		BlockHound.builder()
+			.allowBlockingCallsInside(TemplateEngine.class.getCanonicalName(), "process")
+			.install();
+
 		SpringApplication.run(JubinSpringBootApplication.class, args);
 	}
 
--- src/main/java/com/jubin/springboot/reactive/service/CartService.java
+++ src/main/java/com/jubin/springboot/reactive/service/CartService.java
@@ -17,45 +17,48 @@ import reactor.core.publisher.Mono;
 @Service
 public class CartService {
 	private ItemRepository itemRepository;
-	private ItemByExampleRepository exampleRepository;
 	private CartRepository cartRepository;
+	// private ItemByExampleRepository exampleRepository;
 
 	public CartService(ItemRepository itemRepository,
-		ItemByExampleRepository exampleRepository, CartRepository cartRepository) {
+		CartRepository cartRepository) {
 		this.itemRepository = itemRepository;
-		this.exampleRepository = exampleRepository;
 		this.cartRepository = cartRepository;
 	}
 
 	public Mono<Cart> addToCart(String cartId, String id) {
 		return this.cartRepository.findById(cartId) // <3>
+			.log("find Cart!!!!")
 			.defaultIfEmpty(new Cart(cartId)) //
+			.log("Empty Cart!!!!")
 			.flatMap(cart -> cart.getCartItems().stream() // <4>
 				.filter(cartItem -> cartItem.getItem() //
 					.getId().equals(id)) //
 				.findAny() //
 				.map(cartItem -> {
 					cartItem.increment();
-					return Mono.just(cart);
+					return Mono.just(cart).log("New Cart Item");
 				}).orElseGet(() -> {
 					return this.itemRepository.findById(id) //
+						.log("find Item id = "+id)
 						.map(CartItem::new) // <4>
+						.log("New CartItem")
 						.doOnNext(cartItem -> cart.getCartItems().add(cartItem)) //
 						.map(cartItem -> cart);
 				}))
-			.flatMap(this.cartRepository::save); // <6>
+			.flatMap(this.cartRepository::save).log("Save Item In Cart"); // <6>
 	}
 
-	public Flux<Item> searchByExample(String name, String description, boolean useAnd) {
-		Item item = new Item(name, description, 0.0);
-
-		ExampleMatcher matcher = (useAnd ? ExampleMatcher.matchingAll() : ExampleMatcher.matchingAny())
-			.withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING) // like searching
-			.withIgnoreCase()
-			.withIgnorePaths("price");	// double is not null, ignore 'price' field
-
-		Example<Item> probe = Example.of(item, matcher);
-
-		return exampleRepository.findAll(probe); // query excute
-	}
+	// public Flux<Item> searchByExample(String name, String description, boolean useAnd) {
+	// 	Item item = new Item(name, description, 0.0);
+	//
+	// 	ExampleMatcher matcher = (useAnd ? ExampleMatcher.matchingAll() : ExampleMatcher.matchingAny())
+	// 		.withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING) // like searching
+	// 		.withIgnoreCase()
+	// 		.withIgnorePaths("price");	// double is not null, ignore 'price' field
+	//
+	// 	Example<Item> probe = Example.of(item, matcher);
+	//
+	// 	return exampleRepository.findAll(probe); // query excute
+	// }
 }
